4ab488ff87dff3366aa59ad52194aea3bd868928
xbpneus-gestao-de-frota <catanharacing@gmail.com>
2025-11-06 15:59:07 -0800

    Configure Whitenoise static file handling

--- backend/core/base_settings.py
+++ backend/core/base_settings.py
@@ -136,6 +136,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+if DEBUG:
+    WHITENOISE_USE_FINDERS = True
+    WHITENOISE_AUTOREFRESH = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
