e12a4094a70c3045322a1824429cac5372b0be05
xbpneus-gestao-de-frota <catanharacing@gmail.com>
2025-11-10 10:32:39 -0800

    Cleanup Part 1: remove old infrastructure

--- backend/jobs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ExportJobApi, ExportJobStatusApi, ExportJobDownloadApi
-urlpatterns = [
-    path("export/", ExportJobApi.as_view()),
-    path("<int:job_id>/", ExportJobStatusApi.as_view()),
-    path("<int:job_id>/download/", ExportJobDownloadApi.as_view()),
-]
